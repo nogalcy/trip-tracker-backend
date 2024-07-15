f6ae162132a8cc60acfb81bbe1aeb7de6c4a5e82
Cy Logan <clogan2004@gmail.com>
2024-07-15 00:28:54 -0400

    index.html

--- src/index.js
+++ src/index.js
@@ -49,10 +49,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/logs', logs);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 })
 
 app.use(middlewares.notFound);
