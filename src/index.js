120aa9470ac3c680eedea177340b012e4f319c77
Cy Logan <clogan2004@gmail.com>
2024-07-14 23:48:24 -0400

    routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const path = require('path');
-
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -42,8 +41,6 @@ app.options('*', (req, res) => {
   res.status(204).send(); // No Content
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World!',
@@ -51,6 +48,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/logs', logs);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
